b1699b8c7183c4a81404fbd6a6d5d8d310675d1f
Бостанова Зухра Кемаловна <ZKBostanova@sberbank.ru>
2022-03-02 10:31:11 +0300

    add custom types

--- solution.go
+++ solution.go
@@ -10,19 +10,22 @@ import "math"
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-type myInt int8
-type SidesTriangle myInt
-type SidesSquare myInt
-type SidesCircle myInt
+//type myInt int8
 
-func CalcSquare(sideLen float64, sidesNum myInt) float64 {
+const SidesTriangle int = 3
+const SidesSquare int = 4
+const SidesCircle int = 1
+
+func CalcSquare(sideLen float64, sidesNum int) float64 {
 	switch sidesNum {
 	case 0:
 		return 0
-	case 3:
+	case SidesTriangle:
 		return math.Pow(sideLen, 2) * math.Sqrt(sideLen) / 4
-	case 4:
+	case SidesSquare:
 		return math.Pow(sideLen, 2)
+	case SidesCircle:
+		return math.Pow(sideLen, 2) * math.Pi
 	}
 	return 1
 }
